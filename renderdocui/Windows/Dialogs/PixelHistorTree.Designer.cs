eb6e81c6d56fc8fdf4b67d2026ae61dff7d29aff
baldurk <baldurk@baldurk.org>
2014-09-07 01:11:08 +0100

    dialog

---
+++ renderdocui/Windows/Dialogs/PixelHistorTree.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace renderdocui.Windows.Dialogs
+{
+    partial class PixelHistorTree
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.parent = new System.Windows.Forms.Panel();
+            this.render = new System.Windows.Forms.Panel();
+            this.parent.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // parent
+            // 
+            this.parent.AutoScroll = true;
+            this.parent.Controls.Add(this.render);
+            this.parent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.parent.Location = new System.Drawing.Point(0, 0);
+            this.parent.Name = "parent";
+            this.parent.Size = new System.Drawing.Size(292, 273);
+            this.parent.TabIndex = 0;
+            // 
+            // render
+            // 
+            this.render.BackColor = System.Drawing.Color.Black;
+            this.render.Location = new System.Drawing.Point(0, 0);
+            this.render.Name = "render";
+            this.render.Size = new System.Drawing.Size(2000, 2000);
+            this.render.TabIndex = 0;
+            this.render.Paint += new System.Windows.Forms.PaintEventHandler(this.render_Paint);
+            // 
+            // PixelHistorTree
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.parent);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "PixelHistorTree";
+            this.Text = "Pixel HistorTree";
+            this.parent.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel parent;
+        private System.Windows.Forms.Panel render;
+    }
+}
---
+++ renderdocui/Windows/Dialogs/PixelHistorTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+using renderdocui.Code;
+using renderdoc;
+
+namespace renderdocui.Windows.Dialogs
+{
+    public partial class PixelHistorTree : DockContent
+    {
+        public PixelHistorTree()
+        {
+            InitializeComponent();
+        }
+
+        private void render_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Red, new Rectangle(250, 250, 500, 500));
+        }
+    }
+}
